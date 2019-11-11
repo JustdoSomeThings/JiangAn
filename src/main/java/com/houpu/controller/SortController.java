20d45e7721db5b088f3462d169e0df8005505a7c
wcg <zshog@qq.com>
2019-11-11 19:47:20 +0800

    网迷亲娘

--- src/main/java/com/houpu/controller/SortController.java
+++ src/main/java/com/houpu/controller/SortController.java
@@ -2,10 +2,13 @@ package com.houpu.controller;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.github.pagehelper.StringUtil;
 import com.houpu.model.Employee;
 import com.houpu.model.Sort;
 import com.houpu.service.SortService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -48,7 +51,30 @@ public class SortController {
         result.put("is", "0");
         result.put("msg", "执行成功");
         return  result;
+    }
 
+    @GetMapping("/queryAllIs")
+    public Map<String, Object> queryAllIs(int page, int limit, @RequestParam Integer is){
+        HashMap<String, Object> hashMap = new HashMap<>();
+        if (!StringUtils.isEmpty(is)){
+        hashMap.put("is", is);
+
+        }
+
+
+
+        // 从第page页开始 每页查询limit数据
+            PageHelper.startPage(page, limit);
+            //调用实现类的方法执行业务
+            List<Sort> sorts = sortService.queryAllSortIs(is);
+            //创建一个存储页面信息的对象
+            PageInfo<Sort> pageInfo = new PageInfo<>(sorts, limit);
+            //创建hasMap将MySQL数据给页面
+            HashMap<String, Object> result = new HashMap<>();
+            result.put("code", 0);
+            result.put("data", pageInfo.getList());//页面数据
+            result.put("count", pageInfo.getTotal());//分页总数
+            return result;
     }
 
 }
